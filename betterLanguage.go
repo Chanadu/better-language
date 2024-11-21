e4bca8b5f34826adbe7ad40cb157f2a9a4114f5e
Chanadu <chandu.peddada@gmail.com>
2024-11-20 23:09:18 -0600

    added very basics and .gitignore

---
+++ betterLanguage.go
@@ -0,0 +1,15 @@
+package main
+
+func LineReader() error {
+
+	return nil
+}
+
+func FileReader() error {
+	return nil
+}
+
+func run(source string) error {
+
+	return nil
+}
--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	args := os.Args
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+	if len(args) > 1 {
+		fmt.Println("Usage: gbpl [script file]")
+		os.Exit(2)
 	}
+	if len(args) == 0 { // Line by Line Reader
+		if err := LineReader(); err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+	} else { // File Reader
+		if err := FileReader(); err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+	}
+
+	os.Exit(0) // Success
 }
